734de3bb669efc0d85279baf4b17a729d963b6c7
Daniel Alvarez <daniel.yllanes@hotmail.com>
2026-02-13 11:20:59 -0500

    all data complete

--- src/main.js
+++ src/main.js
@@ -4,22 +4,28 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div style='background:#ddd;
-              width:14in;
-              border:1px solid black;
-              height:8.5in';>
-    <h1>trio of tapas</h1>
+  <div style='
+              // background:#ddd;
+              width:5in;
+              
+              padding-left:0px;
+              padding-top:0px;
+              padding-right:0px;
+              border:1px solid red;
+              height:8in';>
+
+
 
     <div class='item'>
       <div class='left'>
         <span class='name'>burrata</span>
-        <span class='allergies'>(d)</span>
+        <span class='allergies'>(D)</span>
         <span class='description'>
-          peach conserva, balsamic
+          peach conserva, balsamic glaze
         </span><br/>
 
         <span class='name'>cod fish mousse</span>
-        <span class='allergies'>(gl.d)</span>
+        <span class='allergies'>(gl.D)</span>
         <span class='description'>
           puffed pastry
         </span><br/>
@@ -31,17 +37,16 @@ document.querySelector('#app').innerHTML = `
         </span><br/>
 
         <div class='wine'>
-          Xarel lo, Macabeu, Parellada, Monastrell, <span class='wine-name'>De Nit</span>, 
-          2022, Raventós i Blanc, Conca del Riu Anoia
+          Xarel-lo, Macabeu, Parellada, Monastrell, <span class='wine-name'>de Nit Rosé</span>, 
+          2023, Raventós i Blanc, Conca del Riu Anoia, Penedès
         </div>
-      </div>  
-      <div class='right'></div>
+        
+      </div>
     </div>
 
 
 
 
-    <h1>appetizers</h1>
 
     <div class='item'>
       <div class='left'>      
@@ -51,190 +56,288 @@ document.querySelector('#app').innerHTML = `
         balsamic pearls, hazelnut crumble, champagne vinaigrette</span>
         
         <div class='wine'>
-          Hondarrabi Zuri, Hondarrabi Beltza, <span class='wine-name'>Rubentis</span>, 2023, Ameztoi, Getariako Txakolina
+          Sauvignon Blanc, 
+          <span class='wine-name'>Vielles Vignes</span>, 
+          2024, Jean-Marc et Romain Pastou, Sancerre, Loire Valley, France
         </div>
       </div>
-      <div class='right'></div>
+      <div class='right'>
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>mushroom velouté</span>
-        <span class='allergies'>(d.gl.vegetarian)</span>
-        <span class='description'>organic mushrooms, Manchego cheese foam, black truffle,
-        toast</span>
+        <span class='allergies'>(D.gl.vegetarian)</span>
+        <span class='description'>organic mushrooms(shiitake, oyster, brown beech), 
+        Manchego cheese foam, black truffle, toast</span>
         
         <div class='wine'>
-          Hondarrabi Zuri, <span class='wine-name'>Ameztoi</span>, 2023, Getariako Txakolina
+          Palomino, 
+          <span class='wine-name'>Emperatriz Eugenia</span>, 
+          Very Rare Oloroso, Lustau
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>smoked salmon</span>
-        <span class='allergies'>(d.gl)</span>
-        <span class='description'>crème fraîche, cucumber, beets, dill vinaigrette, avocado</span>
+        <span class='allergies'>(d)</span>
+        <span class='description'>
+          crème fraîche, mint, cucumber, beets, dill vinaigrette, 
+          avocado purée, gooseberries</span>
         
         <div class='wine'>
-          Albariño, <span class='wine-name'>Val de Meigas</span>, 2023, Rías Baixas
+          Viura, 
+          <span class='wine-name'>Millenium Brut</span>, 
+          n.v., Bodegas Ondarre, Valle del Ebro
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+        
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
-        <span class='name'>lamb ragù</span>
-        <span class='allergies'>(d)</span>
-        <span class='description'>fresh fusilli pasta, lamb shank, Parmesan cheese foam, truffle dust</span>
+        <span class='name'>pasta and lamb ragù</span>
+        <span class='allergies'>(d.GL)</span>
+        <span class='description'>
+          fresh fusilli pasta, lamb shank ragù, Parmesan cheese foam, truffle dust
+        </span>
         
         <div class='wine'>
-          Tempranillo, Gar., Maz., Gra., <span class='wine-name'>Cerro Añon</span>, Gran Reserva, 2016, Bodegas Olarra, Rioja
+          Tempranillo, 
+          <span class='wine-name'>La Planta</span>, 
+          2023, Ribera del Duero
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+    
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>foie gras</span>
-        <span class='allergies'>(d.gl)</span>
-        <span class='description'>grade A, green apple, membrillo, vanilla, Pedro Ximenez reduction, toast</span>
+        <span class='allergies'>(d.gl.alcohol)</span>
+        <span class='description'>
+          grade A, green apple, membrillo, vanilla, Pedro Ximenez reduction, toast
+        </span>
 
         <div class='wine'>
-          Sémillon, <span class='wine-name'>Cyprès de climens</span>, 2016, Barsac, Sauternes
+          Sémillon, 
+          <span class='wine-name'>Cyprès de climens</span>, 
+          2016, Barsac, Sauternes
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      </div>
     </div>
 
-    <h1>entrées</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div style='background:#ccc;'>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>polenta and vegetables</span>
-        <span class='allergies'>(d.vegetarian)</span>
+        <span class='allergies'>(d.n.vegetarian)</span>
         <span class='description'>
-          creamy polenta, seasonal vegetables, red bell pepper foam, hazelnuts, Parmesan cheese
+          creamy polenta, seasonal vegetables, red bell pepper foam, hazelnuts, 
+          Parmesan cheese
         </span>
 
         <div class='wine'>
-          Garnatxa, Samsó, Cabernet Sauvignon, Syrah, <span class='wine-name'>Nita</span>, 2020, Meritxell Pallejà, Priorat
+          Tibouren, Grenache, 
+          <span class='wine-name'>Cuvée Spéciale Tibouren</span>, 
+          2022, Clos Cibonne, Côtes de Provence
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>Mediterranean sea bass</span>
-        <span class='allergies'>(d)</span>
+        <span class='allergies'></span>
         <span class='description'>
-          saffron potatoes, piperrada (bell peppers, onions, paprika, tomato)
+          saffron potatoes, green peas, 
+          piperrada (bell peppers, onions, paprika, tomato)
         </span>
 
         <div class='wine'>
-          Albariño, <span class='wine-name'>Palacio de Fefiñanes</span>, 2023, Rías Baixas
+          Furmint, 
+          <span class='wine-name'>Mandolás</span>, 
+          2021, Oremus, Tempos Vega Sicilia, Tokaj, Hungary
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>shellfish</span>
-        <span class='allergies'>(d)</span>
+        <span class='allergies'>(D.PORK)</span>
         <span class='description'>
           shrimp, mussels, clams, bay scallops, bacon, fingerling potatoes, celery, 
           carrots, cauliflower, light seafood broth
         </span>
 
         <div class='wine'>
-          Xarel lo, <span class='wine-name'>Gres</span>, 2023, Anima Mundi, Catalunya
+          Albariño, 
+          <span class='wine-name'>Val de Meigas</span>, 
+          2024, Rías Baixas
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+        
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>wagyu bone-in beef short ribs</span>
-        <span class='allergies'>(d)</span>
+        <span class='allergies'>(d.GL)</span>
         <span class='description'>
           potato fondant, broccolini, tomatoes, sweet piquillo pepper, 
           ginger-beer-soy sauce
         </span>
 
         <div class='wine'>
-          Garnatxa, Merlot, Syrah, <span class='wine-name'>Ceps Nous</span>, 2016, Pasanau, Priorat
+          Tempranillo, 
+          <span class='wine-name'>El Pedrosal</span>, 
+          Crianza, 2021, Bodegas Hnos. Pérez Pascuas, Ribera del Duero
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
-        <span class='name'>duck confit</span>
+        <span class='name'>duck leg confit</span>
         <span class='allergies'>(d.gl)</span>
         <span class='description'>
-          creamy farro, sweet potato, artichokes, mushrooms, lavender-citrus gastrique
+          creamy farro, sweet potato purée, artichokes, organic brown beech mushrooms, 
+          lavender-citrus gastrique
         </span>
 
         <div class='wine'>
-          Tempranillo, <span class='wine-name'>Portia</span>, Crianza, 2016, Ribera del Duero
+          Tempranillo, Gar., Maz., Gra., 
+          <span class='wine-name'>Cerro Añon</span>, 
+          Gran Reserva, 2018, Bodegas Olarra, Rioja
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+          
+      </div>
     </div>
 
-<h1>desserts</h1>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <div class='item'>
       <div class='left'>
         <span class='name'>chocolate temptation</span>
-        <span class='allergies'>(d.gl.n)</span>
+        <span class='allergies'>(D.gl.N)</span>
         <span class='description'>
-          hazelnut sablé, vanilla-almond sponge cake, hazelnut crémeux, cinnamon 
-          nougatine, chocolate profiterole
+          hazelnut-cocoa sablé, orange-almond sponge cake, hazelnut crémeux, cinnamon 
+          nougatine, pastry cream profiterole
         </span>
 
         <div class='wine'>
-          Furmint, field blend, <span class='wine-name'>Oremus</span>, 2018, 3 Puttonyos, Tempos Vega Sicilia, Tokaji Aszú
+          Field blend, 
+          <span class='wine-name'>10 Year Tawny Port</span>, 
+          Noval
         </div>
       </div>  
-      <div class='right'></div>
+      <div class='right'>
+      
+      </div>
     </div>
 
     <div class='item'>
       <div class='left'>
         <span class='name'>sweet rose</span>
-        <span class='allergies'>(d.gl.n)</span>
+        <span class='allergies'>(D.gl.N)</span>
         <span class='description'>
           white chocolate mousse, hazelnut brittle, berries compote, 
-          olive oil-poppy seed sponge cake, almond streusel, rose cloud
+          olive oil-poppy seed sponge cake, almond streusel
         </span>
 
         <div class='wine'>
-          Field Blend, <span class='wine-name'>10 Years Tawny Port</span>, Noval
+          Moscatel, 
+          <span class='wine-name'>Selección Especial</span>, 
+          2023, Jorge Ordoñez, Málaga
         </div>
+      </div>  
+      <div class='right'>
+        
+      </div>
+    </div>
+
+    <div class='item'>
+      <div class='left'>
+        <span class='name'>Pineapple Breeze</span>
+        <span class='allergies'></span>
+        <span class='description'>
+          (non-alcoholic); fresh pineapple, fresh lemon juice, mint, lavender syrup
+        </span>
+          <span class='wine-name'>11</span>
       </div>  
       <div class='right'></div>
     </div>
 
     <div class='item'>
       <div class='left'>
-        <span class='name'>Carribean</span>
-        <span class='allergies'>(vegan)</span>
+        <span class='name'>Love Potion</span>
+        <span class='allergies'></span>
         <span class='description'>
-          pineapple rum flambé, virgin piña colada ice cream, 
-          pineapple-lime sponge cake, bittersweet chocolate
+          Mezcal, Tequila, Strega, Aperol, fresh lime juice
         </span>
-
-        <div class='wine'>
-          Moscatel, Jorge Ordoñez, <span class='wine-name'>Selección Especial</span>, 2022, Málaga
-        </div>
+          <span class='wine-name'>17</span>
       </div>  
       <div class='right'></div>
     </div>
